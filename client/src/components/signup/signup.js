24629223ae4424ab94a14280df89d1e31ad04fc3
Josh Improgo <josh.improgo@gmail.com>
2021-04-06 01:12:18 -0400

    Implemented Firebase Authentication

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import './styles/signup.css'
+import firebase from '../../config/firebase-config'
+import { socialMediaAuth } from '../../service/auth'
+import { googleProvider } from '../../config/authentication-methods'
 
 export default function SignUp() {
     const [email, setEmail] = useState('');
@@ -7,43 +10,56 @@ export default function SignUp() {
     const [password, setPassword] = useState('');
     const [error, setError] = useState(''); // for firebase
 
+    const handleOnClick = async provider => {
+        const response = await socialMediaAuth(provider);
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Submitted form");
-        console.log(
-            `email: ${email}\nusername: ${username}\npassword: ${password}`
-        );
+
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(response => console.log(response.user))
+            .catch(error => setError(error.message));
+
     }
 
     const isValid = email !== '' && username !== '' && password !== '';
 
     return (
-        <form className="signup" onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="on"
-                onChange={({ target }) => setEmail(target.value)}
-            />
-            <label htmlFor="username">Username</label>
-            <input
-                type="text"
-                name="username"
-                id="username"
-                autoComplete="off"
-                onChange={({ target }) => setUsername(target.value)}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-                type="password"
-                name="password"
-                id="password"
-                autoComplete="off"
-                onChange={({ target }) => setPassword(target.value)}
-            />
-            <button type="submit" disabled={!isValid}>Sign Up</button>
-        </form>
+        <>
+
+            <form className="signup" onSubmit={handleSubmit}>
+                <div className="error">{error}</div>
+
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="on"
+                    onChange={({ target }) => setEmail(target.value)}
+                />
+                <label htmlFor="username">Username</label>
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="off"
+                    onChange={({ target }) => setUsername(target.value)}
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    onChange={({ target }) => setPassword(target.value)}
+                />
+                <button type="submit" disabled={!isValid}>Sign Up</button>
+                <button onClick={() => handleOnClick(googleProvider)}>Google</button>
+            </form>
+        </>
     )
 }
