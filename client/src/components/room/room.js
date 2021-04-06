e573e6cd26bf3698db2055a0d1a73e89efc22928
Josh Improgo <josh.improgo@gmail.com>
2021-04-06 14:46:57 -0400

    Uninstalled drei package

--- client/src/components/room/room.js
+++ client/src/components/room/room.js
@@ -1,20 +1,69 @@
 import React from 'react'
-import { Canvas } from 'react-three-fiber'
-// import { OrbitControls } from "drei"
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars } from "@react-three/drei"
+import { Physics, useBox, usePlane } from "@react-three/cannon"
+
+import * as THREE from "three"
 import './styles/room.css'
+
 function Box() {
+    const [ref, api] = useBox(() => ({
+        mass: 1,
+        position: [0, 5, 0],
+    }))
     return (
-        <mesh>
-            <boxBufferGeometry attach="geometry" />
-            <meshLambertMaterial attach="material" color="hotpink" />
+        <mesh
+            onClick={() => api.velocity.set(0, 5, 0)}
+            ref={ref}
+            position={[0, 5, 0]}
+            rotation={[0, 1, 0]}
+        >
+            <boxBufferGeometry
+                attach="geometry"
+                args={[1, 2, 2]}
+            />
+            <meshStandardMaterial
+                attach="material"
+                color="hotpink"
+            />
+        </mesh>
+    )
+}
+
+function Plane() {
+    const [ref] = usePlane(() => ({
+        rotation: [-Math.PI / 2, 0, 0],
+    }))
+
+    return (
+        // ref={ref} position={[0, 0, 0]}
+        <mesh
+            rotation={[-Math.PI / 2, 0, 0]}>
+            <planeBufferGeometry
+                attach="geometry"
+                args={[100, 100]}
+            />
+            <meshLambertMaterial
+                attach="material"
+                color="lightblue"
+            />
         </mesh>
     )
 }
 export default function Room() {
     return (
         <Canvas className="canvas">
-            {/* <OrbitControls /> */}
-            <Box />
+            <OrbitControls />
+            <Stars />
+            <ambientLight intensity={0.5} />
+            <spotLight
+                position={[10, 15, 10]}
+                angle={0.3}
+            />
+            <Physics>
+                <Box />
+                <Plane />
+            </Physics>
         </Canvas>
     )
 }
