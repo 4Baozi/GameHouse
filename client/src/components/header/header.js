1bda16ac7c3b912de98ff257d93f0d30e06c3193
Josh Improgo <47151026+josh-improgo@users.noreply.github.com>
2021-04-24 19:10:41 -0400

    Fixed Header Stylings (#2)
    * Modified .gitignore
    
    * Styling of Signin
    
    * Coverage
    
    * Test README.md
    
    * Added stylings to sign-up page
    
    * Navbar fixed position and fill viewport height
    
    * Add Hamburger Reach dependency
    
    * Convert class names to BEM model
    
    * Fix header stylings
    
    * Apply 'sticky' to navbar

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,58 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link,
-    NavLink
+    NavLink,
 } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes'
-import Logo from '../../images/gamehouse_logo.svg'
-import './styles/header.css'
-import './styles/hamburger.css'
+import * as ROUTES from '../../constants/routes';
+import Logo from '../../images/gamehouse_logo.svg';
+import './styles/header.css';
+import { Squash as Hamburger } from 'hamburger-react';
 export default function Header() {
-    const [openToggle, setOpenToggle] = useState(false)
+    const [openToggle, setOpenToggle] = useState(true);
 
     return (
         <nav className='navbar'>
-            <div className='navbar-top'>
-                <NavLink className="navbar-logo-item" exact to={ROUTES.HOME}>
-                    <img className='navbar-logo' src={Logo} alt='Gamehouse' />
+            <div className='navbar__top'>
+                <NavLink className='navbar__logo__link' exact to={ROUTES.HOME}>
+                    <img className='navbar__logo' src={Logo} alt='Gamehouse' />
                 </NavLink>
-                <div className={`navbar-hamburger-icon ${openToggle ? '' : 'open'}`}
-                    onClick={() => setOpenToggle(!openToggle)}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <div className='navbar__hamburger-icon'>
+                    <Hamburger toggled={openToggle} toggle={setOpenToggle} />
                 </div>
             </div>
-            <ul
-                className={`navbar-nav ${openToggle ? '' : 'open-toggle'}`}
-            >
-
-                <li className="navbar-item navbar-btn-item">
-                    <NavLink className="navbar-btn-link" exact to={ROUTES.SIGN_UP}>
-                        <button className="navbar-button">
-                            Sign up
-                         </button>
+            <ul className={`navbar__nav ${!openToggle ? '' : 'open-toggle'}`}>
+                <li className='navbar__nav__item navbar__nav__btn'>
+                    <NavLink
+                        className='navbar-btn-link'
+                        exact
+                        to={ROUTES.SIGN_UP}
+                    >
+                        <button className='navbar__button'>Sign up</button>
                     </NavLink>
                 </li>
-                <li className="navbar-item">
-                    <NavLink className="navbar-link" exact to={ROUTES.ABOUT}>
+                <li className='navbar__nav__item'>
+                    <NavLink
+                        className='navbar__nav__link'
+                        exact
+                        to={ROUTES.ABOUT}
+                    >
                         About
-                     </NavLink>
+                    </NavLink>
                 </li>
-                <li className="navbar-item">
-                    <NavLink className="navbar-link" exact to={ROUTES.BROWSE} >
+                <li className='navbar__nav__item'>
+                    <NavLink
+                        className='navbar__nav__link'
+                        exact
+                        to={ROUTES.BROWSE}
+                    >
                         Gallery
-                     </NavLink>
+                    </NavLink>
                 </li>
-                <li className="navbar-item">
-                    <NavLink className="navbar-link" exact to={ROUTES.CONTACT} >
+                <li className='navbar__nav__item'>
+                    <NavLink
+                        className='navbar__nav__link'
+                        exact
+                        to={ROUTES.CONTACT}
+                    >
                         Contact
-                     </NavLink>
+                    </NavLink>
                 </li>
             </ul>
-        </nav >
-    )
+        </nav>
+    );
 }
