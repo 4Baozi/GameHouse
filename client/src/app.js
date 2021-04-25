0d540c476f0410f910300ffa74d412e0cb02cdc2
Josh Improgo <47151026+josh-improgo@users.noreply.github.com>
2021-04-24 22:06:59 -0400

    Gallery (#8)
    * Added: 2 columns for landing page with static data and basic styling
    
    * Added: responsive text to columns
    
    * Added: Gallery Page
    
    * Added: Gallery
    
    * Updated: Gallery page
    
    * Fix Frame on homepage
    
    Co-authored-by: APiligrim <anastasiyauraleva@gmail.com>

--- client/src/app.js
+++ client/src/app.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink,
 } from 'react-router-dom';
-import { PostsListPage, HomePage, PostFormPage, ShowPostPage, AboutUsPage, SignInPage, SignUpPage, ContactPage } from './pages';
+import {
+    PostsListPage,
+    HomePage,
+    PostFormPage,
+    ShowPostPage,
+    AboutUsPage,
+    SignInPage,
+    SignUpPage,
+    ContactPage,
+    GalleryPage,
+} from './pages';
 import { Header, BottomNav } from './components';
-import * as ROUTES from './constants/routes'
+import * as ROUTES from './constants/routes';
 import './app.css';
 import RoomPage from './pages/roomPage';
 
 export default function App() {
     return (
         <Router>
-          <Header />
-          {/* <div className="container-fluid text-center"> */}
-            {/* <div className="row justify-content-center"> */}
-              <Switch>
+            <Header />
+            <Switch>
                 <Route path={ROUTES.POSTS_FORM} component={PostFormPage} />
                 <Route path={ROUTES.POST_ID} component={ShowPostPage} />
-                <Route path={ROUTES.ABOUT} component={AboutUsPage} />
+                {/* <Route path={ROUTES.ABOUT} component={AboutUsPage} /> */}
+                <Route path={ROUTES.BROWSE} component={GalleryPage} />
                 <Route path={ROUTES.SIGN_IN} component={SignInPage} />
                 <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
                 <Route path={ROUTES.ROOM} component={RoomPage} />
                 <Route path={ROUTES.CONTACT} component={ContactPage} />
                 <Route path={ROUTES.HOME} component={HomePage} />
-              </Switch>
-            {/* </div> */}
-          {/* </div> */}
-          <BottomNav />
+            </Switch>
+            <BottomNav />
         </Router>
     );
 }
--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -1,9 +1,20 @@
-export const HOME = '/'
-export const BROWSE = '/gallery'
-export const ROOM = '/room'
-export const ABOUT = '/about-us'
-export const POST_ID = '/posts/:id'
-export const POSTS_FORM = '/posts/new'
-export const SIGN_UP = '/sign-up'
-export const SIGN_IN = '/sign-in'
-export const CONTACT = '/contact'
+<<<<<<< HEAD
+export const HOME = '/';
+export const BROWSE = '/gallery';
+export const ROOM = '/room';
+export const ABOUT = '/about-us';
+export const POST_ID = '/posts/:id';
+export const POSTS_FORM = '/posts/new';
+export const SIGN_UP = '/sign-up';
+export const SIGN_IN = '/sign-in';
+export const CONTACT = '/contact';
+=======
+export const HOME = "/";
+export const GALLERY = "/gallery";
+export const ROOM = "/room";
+export const ABOUT = "/about-us";
+export const POST_ID = "/posts/:id";
+export const POSTS_FORM = "/posts/new";
+export const SIGN_UP = "/sign-up";
+export const SIGN_IN = "/sign-in";
+>>>>>>> 419f9eee7de3a2c8a4b48e7cba620d65ec28f6c4
