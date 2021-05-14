9e6b851e7cee51d6e37303f0ca161fed4b046e1a
Anastasiya Uraleva <anastasiyauraleva@gmail.com>
2021-05-14 14:54:43 -0400

    Updated: Landing Page Images (#21)
    * Added: favicon
    
    * Added: hero and updated footer
    
    * Updated: Landing page images

--- client/src/components/gallery/galleryCharacter.js
+++ client/src/components/gallery/galleryCharacter.js
@@ -1,21 +1,30 @@
 import React from "react";
 import "./styles/gallery.css";
-import Wizard from "../../../src/images/wizard.gif"
-import Girl from "../../../src/images/girl.gif"
+import Wizard from "../../../src/images/wizard.gif";
+import Girl from "../../../src/images/girl.gif";
 
 function CharacterGallery() {
   return (
     <div>
-        <div class="fix-margin gallery-container people-gallery" >
-            <div class="gallery-card">
-              <img src={Wizard}/>
-              <p class="gallery-card__head"><button className="select-button">Wizard Abe</button></p>
-            </div>
-             <div class="gallery-card">
-              <img src={Girl} />
-              <p class="gallery-card__head"> <button className="select-button">Exo Michelle</button></p>
-            </div>
-          </div>
+      <div class="fix-margin gallery-container people-gallery">
+        <div class="gallery-card">
+          <img src={Wizard} />
+          <p class="gallery-card__head">
+            <a href="http://localhost:3000/room">
+              <button className="select-button">Wizard Abe</button>
+            </a>
+          </p>
+        </div>
+        <div class="gallery-card">
+          <img src={Girl} />
+          <p class="gallery-card__head">
+            {" "}
+            <a href="http://localhost:3000/room">
+              <button className="select-button">Exo Michelle</button>
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
